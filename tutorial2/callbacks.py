392920af7bff8c1ea1e2078ff0da8c5b49a4662c
Fabian Hirschmann <fabian@hirschmann.email>
2016-12-18 15:35:54 +0100

    fix AUC callback for multi input

--- tutorial2/callbacks.py
+++ tutorial2/callbacks.py
@@ -6,6 +6,6 @@ import numpy as np
 
 class AUCHistory(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        y_pred = self.model.predict(self.model.validation_data[0])
-        auc = roc_auc_score(self.model.validation_data[1], y_pred)
+        y_pred = self.model.predict(self.model.validation_data[:-1])
+        auc = roc_auc_score(self.model.validation_data[-1], y_pred)
         print("\nEpoch validation AUC: {}\n".format(auc))
