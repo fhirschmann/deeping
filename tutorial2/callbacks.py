acc394cfd78f2f60bf486dd1abd772af68998e39
Fabian Hirschmann <fabian@hirschmann.email>
2017-01-05 14:53:31 +0000

    some more updates

--- tutorial2/callbacks.py
+++ tutorial2/callbacks.py
@@ -3,9 +3,18 @@ import keras
 from sklearn.metrics import roc_auc_score, confusion_matrix
 import numpy as np
 
-
+        
 class AUCHistory(keras.callbacks.Callback):
+    def __init__(self, input_len=1, *args, **kwargs):
+        self.input_len = input_len
+        super(AUCHistory, self).__init__(*args, **kwargs)
+ 
     def on_epoch_end(self, epoch, logs={}):
-        y_pred = self.model.predict(self.model.validation_data[:-2])
-        auc = roc_auc_score(self.model.validation_data[-2], y_pred)
+        if self.input_len == 1:
+            y_pred = self.model.predict(self.model.validation_data[0])
+        else:
+            y_pred = self.model.predict(self.model.validation_data[:self.input_len])
+        
+        auc = roc_auc_score(self.model.validation_data[self.input_len], y_pred)
         print("\nEpoch validation AUC: {}\n".format(auc))
+
